77acadbacad086caa06e006618616a1cc66bf219
Olivia <olivia.bergaglia@gmail.com>
2025-08-26 16:58:18 +0200

    etat removed from sortie

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 
-use App\Class\Etat;
 use App\Repository\SortieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -36,9 +35,6 @@ class Sortie
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $infosSortie = null;
 
-    #[ORM\Column(enumType: Etat::class)]
-    private Etat $etat = Etat::CREATED;
-
     /**
      * @var Collection<int, Participant>
      */
@@ -52,28 +48,30 @@ class Sortie
 
 
 
+    #[ORM\ManyToOne(inversedBy: 'sorties')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Lieu $Lieu = null;
+
+
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Site $Site = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Etat $etat = null;
+
     public function __construct()
     {
         $this->Site = new ArrayCollection();
         $this->participant = new ArrayCollection();
     }
 
-
-
-    #[ORM\ManyToOne(inversedBy: 'sorties')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Lieu $Lieu = null;
-
-    #[ORM\ManyToOne]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Site $Site = null;
-
     public function getId(): ?int
     {
         return $this->id;
     }
-
-
 
 
     public function getNom(): ?string
@@ -148,17 +146,6 @@ class Sortie
         return $this;
     }
 
-    public function getEtat(): ?Etat
-    {
-        return $this->etat;
-    }
-
-    public function setEtat(Etat $etat): static
-    {
-        $this->etat = $etat;
-
-        return $this;
-    }
 
     /**
      * @return Collection<int, Participant>
@@ -229,6 +216,18 @@ class Sortie
     public function setSite(?Site $Site): static
     {
         $this->Site = $Site;
+
+        return $this;
+    }
+
+    public function getEtat(): ?Etat
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(?Etat $etat): static
+    {
+        $this->etat = $etat;
 
         return $this;
     }
