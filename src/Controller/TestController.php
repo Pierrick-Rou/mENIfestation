eb9078d7118c37dff04f6b2d03725eec3f084439
Maxime JEANNIN <maxime.jeannin@hotmail.fr>
2025-08-26 09:50:46 +0200

    ajout de routes de test dans le TestController, on peux protéger les routes

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,11 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class TestController extends AbstractController
 {
     #[Route('/test', name: 'app_test')]
     public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+    #[Route('/test-admin', name: 'app_test_admin')]
+    #[ISGranted('ROLE_ADMIN')]
+    public function indexAdmin(): Response
     {
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
