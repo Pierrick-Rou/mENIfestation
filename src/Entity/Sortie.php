e6fe256e52de19b3ff13f8d23af3b7974c0536a5
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-08-27 10:50:01 +0200

    ajout de l'inistialisation de l'etat dans la route sorti_create

--- src/Entity/Sortie.php
+++ src/Entity/Sortie.php
@@ -3,7 +3,7 @@
 namespace App\Entity;
 
 
-use App\Class\Etat;
+use App\Entity\Etat;
 use App\Repository\SortieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
