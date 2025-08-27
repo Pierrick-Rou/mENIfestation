45862a4643da1e6a98b9895310e6ebefbb003fe5
Pierrick ROUXEL <pierrick.rouxel2025@campus-eni.fr>
2025-08-27 11:14:53 +0200

    ajout de la suppression de profil(en cours)

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use App\Entity\Participant;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-#[Route('/profil', name : 'profil')]
+#[Route('/profil', name : 'app_profil')]
 final class ProfilController extends AbstractController
 {
-    #[Route('/details/{id}', name: 'app_profil', requirements: ['id' => '\d+'])]
+    #[Route('/details/{id}', name: '_details', requirements: ['id' => '\d+'])]
     public function index(Participant $participant): Response
     {
 
@@ -20,4 +22,17 @@ final class ProfilController extends AbstractController
             'participant' => $participant,
         ]);
     }
+
+    #[Route('/delete/{id}', name: '_delete', requirements: ['id' => '\d+'])]
+
+    public function delete(Participant $participant, EntityManagerInterface $em, Request $request): Response
+    {
+        $this->isCsrfTokenValid('delete'.$participant->getId(), $request->get('token'));
+        $em->remove($participant);
+        $em->flush();
+
+        $this->addFlash('success','le profil à été supprimé ');
+        return $this->redirectToRoute('app_register');
+    }
+
 }
