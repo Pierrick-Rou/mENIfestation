92110093802ad73895ff9297fb8ddb496f51db03
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-08-27 14:50:11 +0200

    ajout des boutons admin menant aux pages de gestion

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,13 +9,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin', name: 'app_admin')]
 final class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(ParticipantRepository $pr, SiteRepository $sr, LieuRepository $lr,): Response
+
+    #[Route('/', name: '_index')]
+    public function index(): Response{
+        return $this->render('admin/index.html.twig', []);
+    }
+
+    #[Route('/utilisateurs', name: '_users')]
+    public function users(ParticipantRepository $pr, SiteRepository $sr, LieuRepository $lr,): Response
     {
         $listeParticipants=$pr->findAll();
-        return $this->render('admin/index.html.twig',['listeParticipants'=>$listeParticipants]);
+        return $this->render('admin/users.html.twig',['listeParticipants'=>$listeParticipants]);
+
+    }
+
+    #[Route('/sites', name: '_sites')]
+    public function sites(SiteRepository $sr): Response
+    {
+        $listeSites=$sr->findAll();
+        return $this->render('admin/sites.html.twig',[
+            'listeSites'=>$listeSites]);
+
+    }
+
+    #[Route('/lieux', name: '_lieux')]
+    public function participants(LieuRepository $lr): Response
+    {
+        $listeLieux=$lr->findAll();
+        return $this->render('admin/lieux.html.twig',[
+            'listeLieux'=>$listeLieux]);
+
     }
 
 
