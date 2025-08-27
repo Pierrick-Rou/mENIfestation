25d3549369061a9303c58c3e906d9fb5fb63cafa
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-08-27 11:57:01 +0200

    ajout liste des participants dans la page /admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,11 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(ParticipantRepository $pr): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        $listeParticipants=$pr->findAll();
+        return $this->render('admin/index.html.twig',['listeParticipants'=>$listeParticipants]);
     }
 
 
