eaaafc2007fe6ad12d08f6268ea907f2c770902c
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-08-27 13:49:12 +0200

    ajour css table

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\LieuRepository;
 use App\Repository\ParticipantRepository;
+use App\Repository\SiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,7 +12,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(ParticipantRepository $pr): Response
+    public function index(ParticipantRepository $pr, SiteRepository $sr, LieuRepository $lr,): Response
     {
         $listeParticipants=$pr->findAll();
         return $this->render('admin/index.html.twig',['listeParticipants'=>$listeParticipants]);
