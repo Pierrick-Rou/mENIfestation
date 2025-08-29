5ca2659ede21c40b08d6ba6159db3dba92d554d3
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-08-29 14:47:11 +0200

    Route calendrier déplacée dans sortiecontroller et ajout d'un isgranted user a la route sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -32,12 +32,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\DelayStamp;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/sortie', name: 'app_sortie_')]
 final class SortieController extends AbstractController
 {
 
     #[Route('', name: 'home')]
+    #[IsGranted('ROLE_USER')]
 
     public function index(Request                $request,
                           SortieRepository       $sortieRepository,
@@ -248,6 +250,52 @@ final class SortieController extends AbstractController
         $em->remove($sortie);
         $em->flush();
         return $this->redirectToRoute('app_sortie_home');
+    }
+
+    #[Route('/calendrier', name: '_cal')]
+    public function calendrier(SortieRepository $sr): Response
+    {
+        setlocale(LC_TIME, 'fr_FR');
+        $dateDuJour =  new \DateTime();
+        $mois=$dateDuJour->format('F');
+        $joursDansLeMois = $dateDuJour->format('t');
+        $moisArr=range(1,$joursDansLeMois);
+
+
+        $dayfunction=function($d): string
+        {
+            setlocale(LC_TIME, 'fr_FR');
+            $date=new \DateTime();
+            $m=$date->format('m');
+            $y=$date->format('Y');
+            return date('l',mktime(0,0,0,$m,$d,$y));
+        };
+
+        $m=$dateDuJour->format('m');
+        $y=$dateDuJour->format('Y');
+        $premierJour=date('w',mktime(0,0,0,$m,1,$y));
+//        dd($jourAvant);
+
+        $days=array_map($dayfunction,$moisArr);
+        $sortiesDuMois=$sr->findByMonth();
+        $arrName=$moisArr;
+        $arrId=$moisArr;
+        foreach ($sortiesDuMois as $sortie) {
+            $arrName[$sortie->getDateHeureDebut()->format('d')]=$sortie->getNom();
+            $arrId[$sortie->getDateHeureDebut()->format('d')]=$sortie->getId();
+        }
+//        dd($days);
+
+
+        return $this->render('sortie/calendrier.html.twig', ['moisArr'=>$moisArr,
+            'dateDuJour'=>$dateDuJour,
+            'nbJours'=>$joursDansLeMois,
+            'jours'=>$days,
+            'mois'=>$mois,
+            'events'=>$arrName,
+            'eventsId'=>$arrId,
+            'premierJour'=>$premierJour,
+        ]);
     }
 
 }
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -32,50 +32,6 @@ final class TestController extends AbstractController
 
 
 
-    #[Route('/calendrier', name: 'app_cal')]
-    public function calendrier(SortieRepository $sr): Response
-    {
-        setlocale(LC_TIME, 'fr_FR');
-        $dateDuJour =  new \DateTime();
-        $mois=$dateDuJour->format('F');
-        $joursDansLeMois = $dateDuJour->format('t');
-        $moisArr=range(1,$joursDansLeMois);
 
-
-        $dayfunction=function($d): string
-        {
-            setlocale(LC_TIME, 'fr_FR');
-            $date=new \DateTime();
-            $m=$date->format('m');
-            $y=$date->format('Y');
-            return date('l',mktime(0,0,0,$m,$d,$y));
-        };
-
-        $m=$dateDuJour->format('m');
-        $y=$dateDuJour->format('Y');
-        $premierJour=date('w',mktime(0,0,0,$m,1,$y));
-//        dd($jourAvant);
-
-        $days=array_map($dayfunction,$moisArr);
-        $sortiesDuMois=$sr->findByMonth();
-        $arrName=$moisArr;
-        $arrId=$moisArr;
-        foreach ($sortiesDuMois as $sortie) {
-            $arrName[$sortie->getDateHeureDebut()->format('d')]=$sortie->getNom();
-            $arrId[$sortie->getDateHeureDebut()->format('d')]=$sortie->getId();
-        }
-//        dd($days);
-
-
-        return $this->render('test/calendrier.html.twig', ['moisArr'=>$moisArr,
-                                                                'dateDuJour'=>$dateDuJour,
-                                                                'nbJours'=>$joursDansLeMois,
-                                                                'jours'=>$days,
-                                                                'mois'=>$mois,
-                                                                'events'=>$arrName,
-                                                                'eventsId'=>$arrId,
-                                                                'premierJour'=>$premierJour,
-        ]);
-    }
 
 }
