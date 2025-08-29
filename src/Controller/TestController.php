950a6cb68059267c1cad8cd4a6cf0dff5cebeded
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-08-29 11:31:49 +0200

    décalage du calendrier

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+
 final class TestController extends AbstractController
 {
     #[Route('/', name: 'app_test')]
@@ -34,23 +35,50 @@ final class TestController extends AbstractController
     #[Route('/calendrier', name: 'app_cal')]
     public function calendrier(SortieRepository $sr): Response
     {
+        setlocale(LC_TIME, 'fr_FR');
         $dateDuJour =  new \DateTime();
-        $mois=$dateDuJour->format('m');
+        $mois=$dateDuJour->format('M');
         $joursDansLeMois = $dateDuJour->format('t');
         $moisArr=range(1,$joursDansLeMois);
+
+
+
+
+
+        $dayfunction=function($d): string
+        {
+            setlocale(LC_TIME, 'fr_FR');
+            $date=new \DateTime();
+            $m=$date->format('m');
+            $y=$date->format('Y');
+
+            return date('l',mktime(0,0,0,$m,$d,$y));
+        };
+
+        $m=$dateDuJour->format('m');
+        $y=$dateDuJour->format('Y');
+        $premierJour=date('w',mktime(0,0,0,$m,1,$y));
+//        dd($jourAvant);
+
+        $days=array_map($dayfunction,$moisArr);
         $sortiesDuMois=$sr->findByMonth();
-        $arr=$moisArr;
+        $arrName=$moisArr;
+        $arrId=$moisArr;
         foreach ($sortiesDuMois as $sortie) {
-//            $event=[];
-//            $event['nom']=$sortie->getNom();
-//            $event['id']=$sortie->getId();
-            $arr[$sortie->getDateHeureDebut()->format('d')]=$sortie->getNom();
-//            $moisArr[$sortie->getDateHeureDebut()->format('d')-1]=$arr;
+            $arrName[$sortie->getDateHeureDebut()->format('d')]=$sortie->getNom();
+            $arrId[$sortie->getDateHeureDebut()->format('d')]=$sortie->getId();
         }
-//        dd($arr);
+//        dd($days);
 
 
-        return $this->render('test/calendrier.html.twig', ['moisArr'=>$moisArr,'dateDuJour'=>$dateDuJour, 'nbJours'=>$joursDansLeMois, 'mois'=>$mois, 'events'=>$arr
+        return $this->render('test/calendrier.html.twig', ['moisArr'=>$moisArr,
+                                                                'dateDuJour'=>$dateDuJour,
+                                                                'nbJours'=>$joursDansLeMois,
+                                                                'jours'=>$days,
+                                                                'mois'=>$mois,
+                                                                'events'=>$arrName,
+                                                                'eventsId'=>$arrId,
+                                                                'premierJour'=>$premierJour,
         ]);
     }
 
