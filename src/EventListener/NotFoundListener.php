1bdc16b1fa7afb9878cdff982bce1f67ca037140
mjeannin2025 <maxime.jeannin@hotmail.fr>
2025-09-02 11:41:47 +0200

    Ajout d'un eventListener pour gérer les exceptions httpnotfound après requête pour un profil

---
+++ src/EventListener/NotFoundListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+#[AsEventListener(event: 'kernel.exception', method: 'onKernelException')]
+class NotFoundListener
+{
+    private $router;
+    public function __construct(UrlGeneratorInterface $router){
+        $this->router = $router;
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        // Vérifie si l'exception est une NotFoundHttpException
+        if ($exception instanceof NotFoundHttpException) {
+            // Vérifie si la requête concerne la route '_details' (optionnel)
+            $request=$event->getRequest();
+            if($request->attributes->get('_route')=='app_profil_details'){
+                $session = $request->getSession();
+                $session->getFlashBag()->add('error', 'Le participant demandé n\'existe pas.');
+            }
+
+            //On redirige
+            $response = new RedirectResponse(
+                $this->router->generate('app_home')
+            );
+            $event->setResponse($response);
+        }
+    }
+}
