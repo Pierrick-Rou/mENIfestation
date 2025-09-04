6ae468b82ae1e3ce74943223c7fe48ba2bf04d4e
Olivia <olivia.bergaglia@gmail.com>
2025-09-04 16:24:19 +0200

    last fix MOTHER FUCKERS !!!!!!!!!!!!!!!!!!!!!!!!!

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,9 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './scriptJS/menuBurger.js';
+import './scriptJS/sciptJS.js'
+import './scriptJS/darkmode.js'
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
